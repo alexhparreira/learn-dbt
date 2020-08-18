296e92b2b7a6d8695750964d214c8e1a3ca4be07
hjones20 <chulahoma1@gmail.com>
2020-08-18 18:11:49 -0400

    Change materialization to tables instead of views, add new dates model

--- first-dbt-project/models/example/cumulative_orders_by_date.sql
+++ first-dbt-project/models/example/cumulative_orders_by_date.sql
@@ -1,5 +1,3 @@
-{{ config(materialized='table') }}
-
 select o_orderdate,
        sum(o_totalprice) over(order by o_orderdate asc rows between unbounded preceding and current row) as cumulative_sales
 from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS"
